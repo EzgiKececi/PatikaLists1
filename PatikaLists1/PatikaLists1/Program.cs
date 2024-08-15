1f2c20ba0c3f1d56dffba60d30e2351a5c9d071a
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-15 13:03:14 +0300

    Add files via upload

---
+++ PatikaLists1/PatikaLists1/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        // Creating a new list object as string type
+        List<string> invitee = new List<string>() { "Bülent Ersoy", "Ajda Pekkan", "Tarkan", "Ebru Gündeş", "Demet Akalın" };
+
+        Console.WriteLine("Davetliler:");
+      
+        //Printing the names of the invitees to the console using a for loop
+        for (int i = 0; i < invitee.Count; i++)
+        {
+            Console.WriteLine(invitee[i]);
+        }
+    
+    }
+}
